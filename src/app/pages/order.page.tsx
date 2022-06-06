7cb42b7cae1869c8eb763f1c382f09af09c033a2
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-06-07 02:59:31 +0300

    order page layout start

--- src/app/pages/order.page.tsx
+++ src/app/pages/order.page.tsx
@@ -1,11 +1,17 @@
+import "./order.page.sass";
+
 const OrderPage = () => {
   return (
     <div className="order-page">
-      <div className="order-page__title">Your order was succesfully created</div>
-      <div className="order-page__subtitle"></div>
+      <div className="order-page__title">
+        Your order was succesfully created
+      </div>
+      <div className="order-page__subtitle">
+        After responding to your offer, you will go to the next page.
+      </div>
       <div className="order-page__state-loading">
-        <div className="order-page__description-text"></div>
-        <div className="order-page__time"></div>
+        <div className="order-page__description-text">Looking for a partner...</div>
+        <div className="order-page__time"> 15:23:16</div>
       </div>
       <div className="order-page__table">
         <div className="order-page__item">
