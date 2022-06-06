0d60c30df5a56349337af67f93f7ff98baed94f8
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-06-06 16:57:11 +0300

    form fix

--- src/app/components/deploy/deploy.form.tsx
+++ src/app/components/deploy/deploy.form.tsx
@@ -1,26 +1,26 @@
-import './deploy.form.sass'
+import "./deploy.form.sass";
 
 const NewOrderForm = () => {
-
-    return (
-        <>
-        <div className="new-order">
-            <div className='new-order__title'> Create your own swap</div>
-            <div className="new-order__form-inner">
-                <span className='new-order__form-tokenInfo'>
-                    <input className="new-order__form-input" placeholder='From'></input>
-                    <select className='new-order__form-select'></select> 
-                </span>
-                <span className='new-order__form-tokenInfo'>
-                    <input className="new-order__form-input" placeholder='To'></input>
-                    <select className='new-order__form-select'></select> 
-                </span>
-                <button className= 'new-order__form-button' placeholder='Create order'>Create new order</button>
-            </div>
+  return (
+    <>
+      <div className="new-order">
+        <div className="new-order__title"> Create your own swap</div>
+        <div className="new-order__form-inner">
+          <span className="new-order__form-token-info">
+            <input className="new-order__form-input" placeholder="From"></input>
+            <select className="new-order__form-select"></select>
+          </span>
+          <span className="new-order__form-token-info">
+            <input className="new-order__form-input" placeholder="To"></input>
+            <select className="new-order__form-select"></select>
+          </span>
         </div>
-        </>
-    )
-}
-
+        <button className="new-order__form-button" placeholder="Create order">
+          Create new order
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default NewOrderForm;
