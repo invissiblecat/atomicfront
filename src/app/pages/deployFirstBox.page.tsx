af416abe734b0549d1e0b9e85abb74cd37914678
Aleksandra Koroleva <aleksandra.koroleva@rsquad.io>
2022-06-11 15:43:26 +0300

    create + select box wip

---
+++ src/app/pages/deployFirstBox.page.tsx
@@ -0,0 +1,35 @@
+import BothBoxSend from "app/components/both-box";
+import BoxSended from "app/components/box-sended";
+import CreateOrder from "app/components/create-order";
+import EndingStage from "app/components/ending-stage";
+import FirstBoxSend from "app/components/first-box";
+import SendTransaction from "app/components/send-transaction";
+import { FC } from "react";
+import { useParams } from "react-router-dom";
+import "./order.page.sass";
+
+type TProps = {
+  title: string;
+  subtitle: string;
+};
+
+const DeployFirstBox: FC<TProps> = ({ title, subtitle }) => {
+  const { id } = useParams<{ id: string }>();
+  
+  return (
+    <div className="order-page">
+      <div className="order-page__title">
+        {title}
+      </div>
+      <div className="order-page__subtitle">
+        {subtitle}
+      </div>
+      <FirstBoxSend />
+      {/* <BothBoxSend /> */}
+      {/* <BoxSended /> */}
+      {/* <EndingStage /> */}
+    </div>
+  );
+};
+
+export default DeployFirstBox;
---
+++ src/app/pages/orderCreated.page.tsx
@@ -0,0 +1,47 @@
+import BothBoxSend from "app/components/both-box";
+import BoxSended from "app/components/box-sended";
+import CreateOrder from "app/components/create-order";
+import EndingStage from "app/components/ending-stage";
+import FirstBoxSend from "app/components/first-box";
+import SendTransaction from "app/components/send-transaction";
+import { FC, useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useGetBoxByIdQuery } from "redux/project.api";
+import "./order.page.sass";
+
+type TProps = {
+  title: string;
+  subtitle: string;
+};
+
+const OrderCreatedPage: FC<TProps> = ({ title, subtitle }) => {
+  const { boxId } = useParams<{ boxId: string }>();
+  const history = useHistory();
+  const {
+    data: box,
+    refetch: refetchProject,
+  } = useGetBoxByIdQuery(boxId, {
+    pollingInterval: 10000,
+  });
+
+  useEffect(() => {
+    console.log(box)
+    if (box && box.reciever !== '') {
+      history.push(`/deploySender/${boxId}`)
+    }
+  }, [box?.reciever]);
+
+  
+  return (
+    <div className="order-page">
+      <div className="order-page__title">
+        {title}
+      </div>
+      <div className="order-page__subtitle">
+        {subtitle}
+      </div>
+    </div>
+  );
+};
+
+export default OrderCreatedPage;
